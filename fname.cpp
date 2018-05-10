9c60e9d458045501123516ca19b393a926a32dfc
Sumi <sumiranwaghmare8@gmail.com>
2018-05-11 01:26:09 +0530

    added first name to identical file

--- fname.cpp
+++ fname.cpp
@@ -1,5 +1,24 @@
 #include<stdio.h>
 int main()
 {
-	//Sumiran 106117101
-}
+	int a[10];
+	for(int i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			if(j!=9)
+			if(a[j]>a[j+1])
+			{
+			int temp=a[j+1];
+			a[j+1]=a[j];
+			a[j]=temp;
+			}
+		}
+	}
+	for(int i=0; i<10; i++)
+	printf("  %d",a[i]);
+}//Sumiran 106117101
